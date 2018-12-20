53134dffef340807f326fcccbb5c1531eea0bc53
Cody Ogden <cody@codyogden.com>
2018-12-20 11:40:14 -0600

    Update card.js

--- bin/card.js
+++ bin/card.js
@@ -1,18 +1,11 @@
 #!/usr/bin/env node
-// 👆 Used to tell Node.js that this is a CLI tool
-
-// Pull in our modules
 const chalk = require('chalk')
 const boxen = require('boxen')
-
-// Define options for Boxen
 const options = {
   padding: 1,
   margin: 1,
   borderStyle: 'round'
 }
-
-// Text + chalk definitions
 const data = {
   name: chalk.white('Cody Ogden /'),
   handle: chalk.cyan('codyogden'),
@@ -29,8 +22,6 @@ const data = {
   labelWeb: chalk.white.bold('       Web:'),
   labelCard: chalk.white.bold('      Card:')
 }
-
-// Actual strings we're going to output
 const newline = '\n'
 const heading = `${data.name} ${data.handle}`
 const working = `${data.labelWork}  ${data.work}`
@@ -40,7 +31,6 @@ const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
 const webing = `${data.labelWeb}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
 
-// Put all our output together into a single variable so we can use boxen effectively
 const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline + carding
 
 console.log(chalk.green(boxen(output, options)))
